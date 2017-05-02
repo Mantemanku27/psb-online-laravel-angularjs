b9870fe914ce962e15c66f8b991242071740079e
Mantemanku27 <mantemanku27@gmail.com>
2017-05-02 12:53:54 +0700

    change main.js

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -43,7 +43,20 @@ function ($rootScope, $state, $stateParams) {
         job: 'ng-Dev',
         picture: 'app/img/user/02.jpg'
     };
-}]);
+}])
+.directive('ngEnter', function () {
+        return function (scope, element, attrs) {
+            element.bind("keydown keypress", function (event) {
+                if (event.which === 13) {
+                    scope.$apply(function () {
+                        scope.$eval(attrs.ngEnter);
+                    });
+
+                    event.preventDefault();
+                }
+            });
+        };
+    })
 // translate config
 app.config(['$translateProvider',
 function ($translateProvider) {
