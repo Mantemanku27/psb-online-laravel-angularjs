fe240d46c170328aca0e47d0b9f6e7fdcf15eb41
Mantemanku27 <mantemanku27@gmail.com>
2017-05-06 10:53:54 +0700

    change label

--- resources/views/login.php
+++ resources/views/login.php
@@ -1,3 +1,13 @@
+<!-- 
+   |||       ||||        |||       |||     || ||||||||||||  ||||||||||  |||       |||        |||       |||     ||
+   ||||     || ||       |||||      ||||    ||      ||       ||          ||||     ||||       |||||      ||||    ||
+   || ||   ||  ||      ||   ||     || ||   ||      ||       ||          || ||   || ||      ||   ||     || ||   ||
+   ||  || ||   ||     ||     ||    ||  ||  ||      ||       ||||||||||  ||  || ||  ||     ||     ||    ||  ||  ||
+   ||   |||    ||    |||||||||||   ||   || ||      ||       ||          ||   |||   ||    |||||||||||   ||   || ||
+   ||    |     ||   ||         ||  ||    ||||      ||       ||          ||    |    ||   ||         ||  ||    ||||
+  ||||        |||| ||||       |||| ||     |||      ||       |||||||||| ||||       |||| ||||       |||| ||     |||
+-->
+
 <!DOCTYPE html>
 <html lang="en" data-ng-app="clipApp">
 <head>
@@ -9,30 +19,29 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
   <meta name="HandheldFriendly" content="true" />
   <meta name="apple-touch-fullscreen" content="yes" />
-  <title data-ng-bind="pageTitle()">Manteman - Angular Bootstrap Admin Template</title>
-  <!--<!— Google fonts —>-->
+  <title data-ng-bind="pageTitle()">Pendaftaran | Login</title>
+  <!-- Import Google fonts -->
   <link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
-  <!--<!— Bootstrap —>-->
+  <!-- Bootstrap -->
   <link rel="stylesheet" href="../bower_components/bootstrap/dist/css/bootstrap.min.css">
-  <!--<!— Font Awesome —>-->
+  <!-- Font Awesome -->
   <link rel="stylesheet" href="../bower_components/font-awesome/css/font-awesome.min.css">
-  <!--<!— Themify Icons —>-->
+  <!-- Themify Icons -->
   <link rel="stylesheet" href="../bower_components/themify-icons/css/themify-icons.css">
-  <!--<!— Loading Bar —>-->
+  <!-- Loading Bar -->
   <link rel="stylesheet" href="../bower_components/angular-loading-bar/build/loading-bar.min.css">
-  <!--<!— Animate Css —>-->
+  <!-- Animate CSS -->
   <link rel="stylesheet" href="../bower_components/animate.css/animate.min.css">
-  <!--<!— Clip-Two CSS —>-->
+  <!-- Assets CSS -->
   <link rel="stylesheet" href="../assets/css/styles.css">
   <link rel="stylesheet" href="../assets/css/plugins.css">
-  <!--<!— X Icon —>-->
+  <!-- X Icon -->
   <link rel="shortcut icon" href="../assets/images/favicon.png" type="x-icon">
-
 </head>
-<!--<!— start: LOGIN —>-->
+<!-- Login -->
 <div class="row">
   <div class="main-login col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
-    <!--<!— start: LOGIN BOX —>-->
+    <!-- box login -->
     <div class="box-login">
       <form class="subscribe" accept-charset="UTF-8" action="api/post-login" method="post">
         <h4>
@@ -46,11 +55,11 @@
 
         <fieldset>
           <legend>
-            Sign in to your account
+            Masuk Ke Akun Anda !
           </legend>
           <p>
             <img class="avatar-login" src="../assets/images/avatar-login.png">
-            Please enter your name and password to log in.
+            Silahkan Masukan Email & Password anda untuk login.
           </p>
           <div class="form-group">
             <span class="input-icon">
@@ -61,15 +70,12 @@
             <span class="input-icon">
               <input type="password" class="form-control password" name="password" placeholder="Password">
               <i class="fa fa-lock"></i>
-              <a class="forgot" ui-sref="login.forgot">
-                I forgot my password
-              </a> </span>
           </div>
           <div class="form-actions">
             <div class="checkbox clip-check check-primary">
               <input type="checkbox" id="remember" value="1">
               <label for="remember">
-                Keep me signed in
+                Biarkan saya tetap masuk.
               </label>
             </div>
             <button type="submit" class="btn btn-primary pull-right">
@@ -77,18 +83,16 @@
             </button>
           </div>
           <div class="new-account">
-            Don't have an account yet?
+            Belum punya aku?
             <a ui-sref="login.registration">
-              Create an account
+              Buat akun sekarang
             </a>
           </div>
         </fieldset>
       </form>
-      <!--<!— start: COPYRIGHT —>-->
-      <!--<!— end: COPYRIGHT —>-->
     </div>
-    <!--<!— end: LOGIN BOX —>-->
+    <!-- /end box login -->
   </div>
 </div>
-<!--<!— end: LOGIN —>-->
+<!-- /end login -->
 </html>
