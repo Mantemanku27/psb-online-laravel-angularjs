50cc4bd63387a9690a84a1a985f1f80279c5c608
Mantemanku27 <mantemanku27@gmail.com>
2017-05-08 08:51:20 +0700

    menambahkan link kembali ke landingpage

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -20,29 +20,29 @@
   <meta name="HandheldFriendly" content="true" />
   <meta name="apple-touch-fullscreen" content="yes" />
   <title data-ng-bind="pageTitle()">Pendaftaran | Sign Up</title>
-  <!--<!— Google fonts —>-->
+  <!-- Import Google fonts -->
   <link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
-  <!--<!— Bootstrap —>-->
+  <!-- Bootstrap -->
   <link rel="stylesheet" href="../bower_components/bootstrap/dist/css/bootstrap.min.css">
-  <!--<!— Font Awesome —>-->
+  <!-- Font Awesome -->
   <link rel="stylesheet" href="../bower_components/font-awesome/css/font-awesome.min.css">
-  <!--<!— Themify Icons —>-->
+  <!-- Themify Icons -->
   <link rel="stylesheet" href="../bower_components/themify-icons/css/themify-icons.css">
-  <!--<!— Loading Bar —>-->
+  <!-- Loading Bar -->
   <link rel="stylesheet" href="../bower_components/angular-loading-bar/build/loading-bar.min.css">
-  <!--<!— Animate Css —>-->
+  <!-- Animate CSS -->
   <link rel="stylesheet" href="../bower_components/animate.css/animate.min.css">
-  <!--<!— Clip-Two CSS —>-->
+  <!-- Assets CSS -->
   <link rel="stylesheet" href="../assets/css/styles.css">
   <link rel="stylesheet" href="../assets/css/plugins.css">
-  <!--<!— X Icon —>-->
+  <!-- X Icon -->
   <link rel="shortcut icon" href="../assets/images/favicon.png" type="x-icon">
-
 </head>
-<!--<!— start: LOGIN —>-->
+<!-- SignUp -->
+<a class="back-landing" href="landingpage">Kembali ke landing page sekarang !</a>
 <div class="row">
   <div class="main-login col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
-    <!--<!— start: LOGIN BOX —>-->
+    <!-- Box SignUp -->
     <div class="box-login">
       <form class="subscribe" accept-charset="UTF-8" action="api/post-login" method="post">
         <fieldset>
@@ -90,11 +90,9 @@
           </div>
         </fieldset>
       </form>
-      <!--<!— start: COPYRIGHT —>-->
-      <!--<!— end: COPYRIGHT —>-->
     </div>
-    <!--<!— end: LOGIN BOX —>-->
+    <!-- /end box signup -->
   </div>
 </div>
-<!--<!— end: LOGIN —>-->
+<!-- /end signup -->
 </html>
